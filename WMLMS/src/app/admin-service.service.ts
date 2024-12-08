8c725c7c40ed9f3f162f395b7af48659a18a8459
MAT34525 <123415140+MAT34525@users.noreply.github.com>
2024-12-08 15:51:23 +0100

    Admin user table and styling fix

--- WMLMS/src/app/admin-service.service.ts
+++ WMLMS/src/app/admin-service.service.ts
@@ -1,17 +1,34 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface user {
+  user_id : Number,
+  username : String,
+  password : String,
+  email : String,
+  full_name : String
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminServiceService {
 
-  private readonly httpClient = inject(HttpClient);
+  httpClient = inject(HttpClient);
 
-  constructor() { }
+  constructor() {}
 
-  async getUsers()
+  getUsers() : Observable<user[]>
   {
-    return this.httpClient.get('/admin/user-list');
+    return this.httpClient.get<user[]>('/admin/user-list');
+  }
+
+  getUser() : Number
+  {
+    this.httpClient.get('/admin/user-list/:id', {params : { id: 1 }});
+    return 0;
   }
 }
+
--- WMLMS/src/app/admin-user-page/admin-user-page.component.ts
+++ WMLMS/src/app/admin-user-page/admin-user-page.component.ts
@@ -1,26 +1,58 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router'
-import {AdminServiceService} from '../admin-service.service';
+import {AdminServiceService, user} from '../admin-service.service';
+import {
+  MatColumnDef,
+  MatHeaderCell,
+  MatTable, MatTableModule
+} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-admin-user-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatTableModule,
+    MatButtonModule,
+    MatPaginatorModule
+  ],
   templateUrl: './admin-user-page.component.html',
   styleUrl: './admin-user-page.component.css'
 })
-export class AdminUserPageComponent {
+export class AdminUserPageComponent implements OnInit{
 
   private readonly adminService = inject(AdminServiceService);
 
+  userList: user[];
   constructor(private route : Router) {
-
+    this.userList = [];
   }
+
+  // Table
+  displayedColumns: string[] = ['USER_ID', 'USERNAME', 'EMAIL', 'FULL_NAME', 'COMMANDS'];
+
+  // @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit() {
-
+    this.getUsers();
+    // this.dataSource.paginator = this.paginator;
   }
 
+  getUsers()
+  {
+    this.adminService.getUsers().subscribe({
+      next: data => {
+        this.userList = data;
+        console.log('get Http /admin/user-list', data);
+      }, error:err=> {
+        console.log("Failed to load Lessons, saving to component field");
+      }
+    });
 
+  }
 
 }
