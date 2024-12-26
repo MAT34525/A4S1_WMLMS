1298c7ce4bc1732a7ccfef1b1b5d5169bceaf3d0
MAT34525 <123415140+MAT34525@users.noreply.github.com>
2024-12-26 11:30:30 +0100

    Navbar style and user page presentation

--- WMLMS_Frontend/src/app/admin-page-navbar/admin-page-navbar.component.ts
+++ WMLMS_Frontend/src/app/admin-page-navbar/admin-page-navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {NgbDropdown, NgbDropdownMenu, NgbDropdownToggle, NgbNavLink} from "@ng-bootstrap/ng-bootstrap";
 import {MatAnchor} from '@angular/material/button';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
 
 
 @Component({
@@ -13,7 +14,9 @@ import {MatAnchor} from '@angular/material/button';
     RouterLink,
     NgbNavLink,
     RouterOutlet,
-    MatAnchor
+    MatAnchor,
+    MatButtonToggleGroup,
+    MatButtonToggle
   ],
   templateUrl: './admin-page-navbar.component.html',
   standalone: true,
